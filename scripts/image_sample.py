49496e9c7800dc310715e3d9ad7bde097c0fb3ce
maxization <damian.opoka99@gmail.com>
2023-08-20 22:55:10 +0200

    fix imports

--- scripts/image_sample.py
+++ scripts/image_sample.py
@@ -2,7 +2,9 @@
 Generate a large batch of image samples from a model and save them as a large
 numpy array. This can be used to produce samples for FID evaluation.
 """
-
+import sys
+sys.path.append("..")
+sys.path.append(".")
 import argparse
 import os
 
